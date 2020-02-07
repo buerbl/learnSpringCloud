2b0a9af19667d1f635728b9456e7a47c03a73e4c
buer <18319748706@163.com>
2020-02-07 23:29:56 +0800

    :star:调整 maven 依赖冲突

--- microservicecloud-provider-dept-8002/src/main/java/com/example/AppApllcation8002.java
+++ microservicecloud-provider-dept-8002/src/main/java/com/example/AppApllcation8002.java
@@ -5,8 +5,8 @@ import com.example.sercive.IDeptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +17,7 @@ import java.util.List;
  */
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class AppApllcation8002
 {
     public static void main( String[] args ) {
---
+++ microsevicecloud-eureka-7001/src/main/java/com/example/AppApplication7001.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class AppApplication7001
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AppApplication7001.class, args);
+    }
+}
