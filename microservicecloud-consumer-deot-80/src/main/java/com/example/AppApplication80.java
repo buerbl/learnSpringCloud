bb7817b7aa4f0995df0d1e29cf8b8e5a3e013cc8
buer <18319748706@163.com>
2020-02-12 10:36:12 +0800

    :star:整合 ribbon

--- microservicecloud-consumer-deot-80/src/main/java/com/example/AppApplication80.java
+++ microservicecloud-consumer-deot-80/src/main/java/com/example/AppApplication80.java
@@ -4,6 +4,7 @@ import com.example.entity.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +19,14 @@ import java.util.List;
  */
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class AppApplication80
 {
     public static void main( String[] args ) {
         SpringApplication.run(AppApplication80.class, args);
     }
 
-    public static final String URL = "http://localhost:8002";
+    public static final String URL = "http://microservicloud-dept";
 
     @Autowired
     private RestTemplate restTemplatel;
