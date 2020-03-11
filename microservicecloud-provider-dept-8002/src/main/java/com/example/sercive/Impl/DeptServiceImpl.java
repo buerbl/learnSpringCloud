3dd975de08998ad4cc57443d9bdb522edfd219a5
buerbl <18319748706@163.com>
2020-03-11 12:17:35 +0800

    :memo:类名修改

--- microservicecloud-provider-dept-8002/src/main/java/com/example/sercive/Impl/DeptServiceImpl.java
+++ microservicecloud-provider-dept-8002/src/main/java/com/example/sercive/Impl/DeptServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.List;
  * @Date: 2020/2/6 22:56
  */
 @Service
-public class DeptService implements IDeptService {
+public class DeptServiceImpl implements IDeptService {
     @Autowired
     private DeptMapper deptMapper;
     @Override
