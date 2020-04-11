16507b8af8b1ecbbe9b895b67c37801c9bd553c1
buerbl <18319748706@163.com>
2020-04-11 16:36:16 +0800

    :star:hystrix监控

--- microservicecloud-provider-hystrix-8005/src/main/java/com/example/AppApllcation8005.java
+++ microservicecloud-provider-hystrix-8005/src/main/java/com/example/AppApllcation8005.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
 @EnableDiscoveryClient
+@EnableHystrixDashboard
 public class AppApllcation8005 {
     public static void main( String[] args ) {
         SpringApplication.run(AppApllcation8005.class, args);
--- microservicecloud-provider-hystrix-8005/src/main/java/com/example/controller/HystrixController.java
+++ microservicecloud-provider-hystrix-8005/src/main/java/com/example/controller/HystrixController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HystrixController {
 
-    @GetMapping("/hystrix")
+    @GetMapping("/hystrix1")
     @HystrixCommand(fallbackMethod = "fallback")
     public String hystrix() {
         int i = 1 / 0;
